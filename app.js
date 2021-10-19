0ccb85662061cdf22ae7b157fe4e1782ef57c9e1
Daniela <daniela.leloutre@gmail.com>
2021-10-18 23:39:24 -0300

    swagger y pgadmin

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-const express = require('express');
-const { Client } = require('pg');
+const express = require("express");
+const { Client } = require("pg");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const gateway = require("./infrastructure/routes/gateway");
 
 const app = express();
 
-var client;
-if (process.env.NODE_ENV !== 'stage') {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      query_timeout: 1000,
-      statement_timeout: 1000,
-      ssl: false
-    }); 
+let client;
+if (process.env.NODE_ENV !== "stage") {
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    query_timeout: 1000,
+    statement_timeout: 1000,
+    ssl: false,
+  });
 } else {
-    client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      query_timeout: 1000,
-      statement_timeout: 1000,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    });
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    query_timeout: 1000,
+    statement_timeout: 1000,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 }
 
 client.connect();
 
-app.get('/ping', (req, res) => res.send('Pong!'));
+app.use("/gateway", gateway);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get('/status', (req, res) => 
-    client.query('SELECT NOW()', (err) => res.send({ service: 'UP', db: err ? 'DOWN' : 'UP' }))
-);
+app.get("/ping", (req, res) => res.send("Pong!"));
+
+app.get("/status", (req, res) => client.query("SELECT NOW()", (err) => res.send({ service: "UP", db: err ? "DOWN" : "UP" })));
 
 app.listen(process.env.PORT, () => {
-    console.log(`App running on port ${process.env.PORT}`); 
+  // console.log(`App running on port ${process.env.PORT}`);
 });
