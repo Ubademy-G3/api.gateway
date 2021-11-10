da369d6bea97908f623b2f52f1c825666698c42a
Daniela <daniela.leloutre@gmail.com>
2021-11-09 23:19:34 -0300

    fix linter

--- application/controllers/GatewayController.js
+++ application/controllers/GatewayController.js
@@ -1,31 +1,30 @@
 const axios = require("axios");
-const serializer = require('../serializers/LoggedUserSerializer');
+const serializer = require("../serializers/LoggedUserSerializer");
 
 exports.signup = async (req, res) => {
   try {
     const authUser = await axios.post(`${process.env.AUTH_SERVICE_URL}/authorization`, req.body);
-    const user = await axios.post(`${process.env.USERS_SERVICE_URL}/users`, req.body, { headers: {'Authorization': process.env.USERS_APIKEY} });
-    res.status(200).json(authUser.data);
+    await axios.post(`${process.env.USERS_SERVICE_URL}/users`, req.body, { headers: { Authorization: process.env.USERS_APIKEY } });
+    return res.status(200).json(authUser.data);
   } catch (err) {
     if (err.response && err.response.status && err.response.data) {
       return res.status(err.response.status).json(err.response.data);
     }
-    res.status(500).json({ error: "Internal server error" });
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
 
 exports.login = async (req, res) => {
   try {
     const authUser = await axios.post(`${process.env.AUTH_SERVICE_URL}/authentication`, req.body);
-    const user = await axios.get(`${process.env.USERS_SERVICE_URL}/users`, { params: {email: req.body.email}, headers: {'Authorization': process.env.USERS_APIKEY} });
+    const user = await axios.get(`${process.env.USERS_SERVICE_URL}/users`, { params: { email: req.body.email }, headers: { Authorization: process.env.USERS_APIKEY } });
     const loggedUser = Object.assign(authUser.data, user.data[0]);
-    res.status(200).json(serializer(loggedUser));
+    return res.status(200).json(serializer(loggedUser));
   } catch (err) {
-    console.log(err)
     if (err.response && err.response.status && err.response.data) {
       return res.status(err.response.status).json(err.response.data);
     }
-    res.status(500).json({ error: "Internal server error" });
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
 
--- application/serializers/LoggedUserSerializer.js
+++ application/serializers/LoggedUserSerializer.js
@@ -1,19 +1,18 @@
 const serializeUser = (user) => ({
-    id: user.id,
-    token: user.token,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    rol: user.rol
-  });
-  
-  module.exports = (data) => {
-    if (!data) {
-      throw new Error("Expect data to be not undefined nor null");
-    }
-  
-    if (Array.isArray(data)) {
-      return data.map(serializeUser);
-    }
-    return serializeUser(data);
-  };
-  
+  id: user.id,
+  token: user.token,
+  firstName: user.firstName,
+  lastName: user.lastName,
+  rol: user.rol,
+});
+
+module.exports = (data) => {
+  if (!data) {
+    throw new Error("Expect data to be not undefined nor null");
+  }
+
+  if (Array.isArray(data)) {
+    return data.map(serializeUser);
+  }
+  return serializeUser(data);
+};
