a3e369b435f1fd7b8f06b099b13fa079e27ea804
Daniela <daniela.leloutre@gmail.com>
2021-11-11 00:18:46 -0300

    agrego get y patch de usuario por id

--- application/middlewares/AuthMiddleware.js
+++ application/middlewares/AuthMiddleware.js
@@ -2,22 +2,20 @@ const axios = require("axios");
 
 exports.verifyToken = (req, res, next) => {
   try {
-    // console.log(req.headers);
     const token = req.headers.authorization;
     axios.get(`${process.env.AUTH_SERVICE_URL}/authentication`, { params: { token } })
       .then((response) => {
-        // console.log(response);
-        res.status(200).json(response);
+        if (response.data.message === "Invalid token") {
+          res.status(401).json({ error: "Unauthorized" });
+        }
         next();
       })
       .catch((err) => {
-        // console.log(err);
-        res.status(401).json({ error: new Error(`Unauthorized ${err}`) });
+        res.status(401).json({ error: `Unauthorized ${err}` });
       });
   } catch (e) {
-    // console.log(e);
     res.status(401).json({
-      error: new Error("Authentication failed"),
+      error: "Authentication failed",
     });
   }
 };
