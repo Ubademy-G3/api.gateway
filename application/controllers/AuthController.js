3d731ec7be918fdc16c1110d310151514201653d
GonzaloDiazMulligan <gonzalodiazmulligan@gmail.com>
2021-12-05 16:30:26 -0300

    auth passes though admin

--- application/controllers/AuthController.js
+++ application/controllers/AuthController.js
@@ -13,8 +13,17 @@ const subscriptionAboutToExpire = (user) => {
 
 exports.signup = async (req, res) => {
   try {
-    await axios.post(`${process.env.AUTH_SERVICE_URL}/authorization`, req.body);
-    await axios.post(`${process.env.USERS_SERVICE_URL}/users`, req.body, { headers: { Authorization: process.env.USERS_APIKEY } });
+    const result = await axios.get(`${process.env.ADMIN_SERVICE_URL}/microservices/name/?name_list=auth&name_list=users`, { headers: { apikey: process.env.ADMIN_APIKEY } });
+    const auth = result['data']['microservices'][0];
+    const users = result['data']['microservices'][1];
+    if (auth['state'] != 'active'){
+      return res.status(400).json({message: auth['name'] + " microservice is " + auth['state']});
+    }
+    if (users['state'] != 'active'){
+      return res.status(400).json({message: users['name'] + " microservice is " + users['state']});
+    }
+    await axios.post(`${auth['apikey']}/authorization`, req.body);
+    await axios.post(`${process.env.USERS_SERVICE_URL}/users`, req.body, { headers: { Authorization: users['apikey'] } });
     return res.status(200).json({ message: "User created successfully" });
   } catch (err) {
     if (err.response && err.response.status && err.response.data) {
@@ -26,12 +35,21 @@ exports.signup = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
+    const result = await axios.get(`${process.env.ADMIN_SERVICE_URL}/microservices/name/?name_list=auth&name_list=users`, { headers: { apikey: process.env.ADMIN_APIKEY } });
+    const auth = result['data']['microservices'][0];
+    const users = result['data']['microservices'][1];
+    if (auth['state'] != 'active'){
+      return res.status(400).json({message: auth['name'] + " microservice is " + auth['state']});
+    }
+    if (users['state'] != 'active'){
+      return res.status(400).json({message: users['name'] + " microservice is " + users['state']});
+    }
     const authUser = await axios.post(`${process.env.AUTH_SERVICE_URL}/authentication`, req.body);
-    const user = await axios.get(`${process.env.USERS_SERVICE_URL}/users`, { params: { email: req.body.email }, headers: { Authorization: process.env.USERS_APIKEY } });
+    const user = await axios.get(`${process.env.USERS_SERVICE_URL}/users`, { params: { email: req.body.email }, headers: { Authorization: users['apikey'] } });
     const loggedUser = Object.assign(authUser.data, user.data[0]);
     if (subscriptionHasExpired(loggedUser) && loggedUser.subscription !== FREE_SUBSCRIPTION) {
       loggedUser.subscriptionState = "expired";
-      await axios.patch(`${process.env.USERS_SERVICE_URL}/users/${loggedUser.id}`, { params: { subscription: FREE_SUBSCRIPTION } }, { headers: { Authorization: process.env.USERS_APIKEY } });
+      await axios.patch(`${process.env.USERS_SERVICE_URL}/users/${loggedUser.id}`, { params: { subscription: FREE_SUBSCRIPTION } }, { headers: { Authorization: users['apikey'] } });
     } else if (subscriptionAboutToExpire(loggedUser)
       && loggedUser.subscription !== FREE_SUBSCRIPTION) {
       loggedUser.subscriptionState = "about_to_expire";
@@ -48,15 +66,18 @@ exports.login = async (req, res) => {
 };
 
 exports.resetPassword = async (req, res) => {
-  axios.post(`${process.env.AUTH_SERVICE_URL}/authentication/password`, req.body)
-    .then((response) => {
-      res.status(response.status).json(response.data);
-    })
-    .catch((err) => {
-      if (err.response && err.response.status && err.response.data) {
-        res.status(err.response.status).json(err.response.data);
-      } else {
-        res.status(500).json({ error: "Internal server error" });
-      }
-    });
+  try {
+    const result = await axios.get(`${process.env.ADMIN_SERVICE_URL}/microservices/name/auth`, { headers: { apikey: process.env.ADMIN_APIKEY } });
+    const auth = result['data'];
+    if (auth['state'] != 'active'){
+      return res.status(400).json({message: auth['name'] + " microservice is " + auth['state']});
+    }
+    response = await axios.post(`${process.env.AUTH_SERVICE_URL}/authentication/password`, req.body);
+    return res.status(response.status).json(response.data);
+  } catch (err) {
+    if (err.response && err.response.status && err.response.data) {
+      return res.status(err.response.status).json(err.response.data);
+    }
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
