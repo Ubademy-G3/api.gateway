a027122f85f5b1df342a000db73f478a9b48de9f
GonzaloDiazMulligan <gonzalodiazmulligan@gmail.com>
2021-12-22 12:30:07 -0300

    favorites with ratings

--- infrastructure/routes/gateway.js
+++ infrastructure/routes/gateway.js
@@ -20,6 +20,7 @@ router.route("/users/:id").get(UsersController.getUserById);
 router.route("/users/:id").patch(UsersController.updateUserInfo);
 router.route("/users/:id/courses").get(UsersController.getCourses);
 router.route("/users/:id/favorites").get(UsersController.getFavoriteCourses);
+router.route("/users/:id/favorites/rated").get(UsersController.getFavoriteCoursesWithRating);
 router.route("/users/:id/solved-exams").get(UsersController.getSolvedExams);
 router.route("/users/:id/solved-exams/:course_id").get(UsersController.getSolvedExamsByCourse);
 router.route("/users/:id/exams").get(UsersController.getExams);
